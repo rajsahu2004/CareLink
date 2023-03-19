f49a4f63cd400aa1de1420810e91e00916a63229
Raj Sahu <sahuraj457@gmail.com>
2023-03-19 15:08:08 +0530

    base footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+function Footer(){
+    return (
+        <div className="footer-container">
+            <div className="footer-items">
+                <div className="footer-list">
+                    <h3>CareLink</h3>
+                    <ul>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Career</li>
+                        <li>Press</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div className="footer-image-section">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
