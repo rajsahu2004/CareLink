11b25a4c8d583a5c458001617efde5aa869c30dd
Raj Sahu <sahuraj457@gmail.com>
2023-03-19 14:57:39 +0530

    base app.js added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Starting <code>npm start</code>
         </p>
         <a
           className="App-link"
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
